585178000d6b0de1bbec5122db18f7cd42f642b3
LeonardoContino <leoconti2000@hotmail.it>
2022-12-23 16:59:04 +0100

    stampato su js

--- js/main.js
+++ js/main.js
@@ -38,43 +38,74 @@ const posts = [
     id: 1,
     nome: "jack",
     photoprofile: "https://unsplash.it/300/300?image=15",
-    date: 11 / 24 / 2000,
+    date: "11/24/2000",
     text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto.Et nihil ullam aut alias.",
-    imgpost: "https://unsplash.it/300/300?image=15",
+    imgpost: "https://unsplash.it/600/300?image=171",
     likes: 80,
   },
   {
     id: 2,
     nome: "leo",
     photoprofile: "https://unsplash.it/300/300?image=15",
-    date: 10 / 14 / 2001,
+    date: "10/14/2001",
     text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto.Et nihil ullam aut alias.",
-    imgpost: "https://unsplash.it/300/300?image=15",
+    imgpost: "https://unsplash.it/600/300?image=171",
     likes: 50,
   },
   {
     id: 3,
     nome: "franco",
     photoprofile: "https://unsplash.it/300/300?image=15",
-    date: 01 / 20 / 2002,
+    date: "01/20/2002",
     text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto.Et nihil ullam aut alias.",
-    imgpost: "https://unsplash.it/300/300?image=15",
+    imgpost: "https://unsplash.it/600/300?image=171",
     likes: 150,
   },
 ];
 
 //dichiaro elementi
-const postIcon = document.querySelector(".post-meta__icon");
+const post = document.querySelector(".post");
 
 let postElement = "";
 
-posts.forEach((post, i) => {
+posts.forEach((post) => {
   //   console.log(post);
-  const imgprofile = `<img src="${post.photoprofile}">`;
+  const imgprofile = `<img class="profile-pic" src="${post.photoprofile}">`;
+  const userprofile = `<div class="post-meta__author">${post.nome}</div>`;
+  const datauser = `<div class="post-meta__data">${post.date}</div>`;
+  const userText = `<div class="post__text">${post.text}</div>`;
+  const postimage = `<img src="${post.imgpost}" alt="" />`;
+  const likepost = `<b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+`;
   postElement += ` 
-  <div class="post-meta">
-  <div class="post-meta__icon">${imgprofile}</div>
-</div>`;
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">${imgprofile}</div>
+                    <div class="post-meta__data">${datauser} ${userprofile}
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>
+            </div>
+            <div>${userText}</div>
+            <div class="post__image">${postimage}</div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <button
+                            class="like-button js-like-button" href="#"data-postid="1">
+                        <i
+                            class="like-button__icon fas fa-thumbs-up"
+                            aria-hidden="true"
+                                ></i>
+                        <span class="like-button__label">Mi Piace</span>
+                        </button>
+                    </div>
+                    <div class="likes__counter">Piace a ${likepost}</div>
+                    
+                </div>
+            </div>
+
+        </div>`;
 });
 
-postIcon.innerHTML = postElement;
+post.innerHTML = postElement;
